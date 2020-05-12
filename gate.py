49e6348a003ac07192eff50dab37bd5d3939a1b4
Mohin Paramasivam <mohin2099@gmail.com>
2020-05-12 11:03:40 +0800

    Update gate.py

--- gate.py
+++ gate.py
@@ -9,7 +9,7 @@ print ("            #                   Logic Gate                    	#" )
 print ("            #                                                   #" )                          
 print ("            #             Automated Calculation Tool         	#" )        
 print ("            #                                                   #" )        
-print ("            #                   Version 1.0                   	#" )        
+print ("            #                   Version 2.0                   	#" )        
 print ("            #                                                   #" )       
 print ("            #            Author : Mohin Paramasivam             #" )       
 print ("            #                                                   #" )       
@@ -20,216 +20,217 @@ print ("            #####################################################" )
 print("                                                                   ")
 
 
-#Get type of gates input from the user
+end = "no"
 
-gate=input("Please enter the name of logic gate : ")
-
-##Get binary input from user
-
-binary_A=input("Please enter the binary A input : ")
-
-binary_B=input("Please enter the binary B input : ")
+while (end=="no" or end=="No" or end=="NO"):
 
 
-## Array for binary
+	#Get type of gates input from the user
 
-binary_array_A=[]
+	gate=input("Please enter the name of logic gate : ")
 
-binary_array_B=[]
+	##Get binary input from user
 
+	binary_A=input("Please enter the binary A input : ")
 
-#Check length of binary input for counter
-
-binary_length_A=len(binary_A)
-binary_length_B=len(binary_B)
+	binary_B=input("Please enter the binary B input : ")
 
 
-#specify counter for loop
+	## Array for binary
 
-counter_A=0
+	binary_array_A=[]
 
-while(counter_A!=binary_length_A):
-	binary_array_A.append(binary_A[counter_A])
-	counter_A=counter_A+1
+	binary_array_B=[]
 
 
-counter_B=0
+	#Check length of binary input for counter
 
-while(counter_B!=binary_length_B):
-	binary_array_B.append(binary_B[counter_B])
-	counter_B=counter_B+1
-
+	binary_length_A=len(binary_A)
+	binary_length_B=len(binary_B)
 
 
+	#specify counter for loop
 
-# Calculation for Logic Gate
+	counter_A=0
 
-if (gate=="AND" or gate=="and" or gate=="And"):
-	formula="X = A * B "
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
-
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		binary_X=int(binary_array_A[calculation_counter])*int(binary_array_B[calculation_counter])
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
-	
-	print("")
-	print("")
+	while(counter_A!=binary_length_A):
+		binary_array_A.append(binary_A[counter_A])
+		counter_A=counter_A+1
 
 
-elif (gate=="OR" or gate=="or" or gate=="Or"):
-	formula="X = A + B "
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
+	counter_B=0
 
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
-		if binary_X==2:
-			binary_X=1
-		else:
-			binary_X=binary_X
+	while(counter_B!=binary_length_B):
+		binary_array_B.append(binary_B[counter_B])
+		counter_B=counter_B+1
 
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
-	
-	print("")
-	print("")
 
-elif (gate=="NOT" or gate=="not" or gate=="Not"):
-	formula="X = A Inverted "
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
 
-	while(calculation_counter!=len(binary_array_A) ):
-		binary_X=int(binary_array_A[calculation_counter])
-		if(binary_X==1):
-			binary_X=0
-		elif(binary_X==0):
-			binary_X=1
 
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
-	
-	print("")
-	print("")
+	# Calculation for Logic Gate
 
-elif (gate=="NAND" or gate=="nand" or gate=="Nand"):
-	formula="X = A * B (Inverted) "
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
+	if (gate=="AND" or gate=="and" or gate=="And"):
+		formula="X = A * B "
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
 
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		binary_X=int(binary_array_A[calculation_counter])*int(binary_array_B[calculation_counter])
-		if(binary_X==1):
-			binary_X=0
-		elif(binary_X==0):
-			binary_X=1
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
+			binary_X=int(binary_array_A[calculation_counter])*int(binary_array_B[calculation_counter])
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
 
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
-	
-	print("")
-	print("")
 
-elif (gate=="NOR" or gate=="nor" or gate=="Nor"):
-	formula="X = A + B  (Inverted)"
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
 
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
-		if binary_X==2:
-			binary_X=0
-		elif binary_X==1:
-			binary_X=0
-		else:
-			binary_X=1
+	elif (gate=="OR" or gate=="or" or gate=="Or"):
+		formula="X = A + B "
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
 
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
-	
-	print("")
-	print("")
-
-elif (gate=="XOR" or gate=="xor" or gate=="Xor"):
-	formula="X = A + B (Exclusive) "
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
-
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		if(int(binary_array_A[calculation_counter])==1 and int(binary_array_B[calculation_counter])==1):
-			binary_X=0
-		elif(int(binary_array_A[calculation_counter])==0 and int(binary_array_B[calculation_counter])==0):
-			binary_X=0
-		else:
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
 			binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
-		
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
+			if binary_X==2:
+				binary_X=1
+			else:
+				binary_X=binary_X
+
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
+
 	
-	print("")
-	print("")
 
+	elif (gate=="NOT" or gate=="not" or gate=="Not"):
+		formula="X = A Inverted "
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
 
-elif (gate=="XNOR" or gate=="xnor" or gate=="Xnor"):
-	formula="X = A + B (Inverted Exclusive)"
-	calculation_counter=0
-	print("")
-	print("Gate 		: ",gate.upper())
-	print("")
-	print("Formula 	: ",formula)
-	print("")
-	print("X 		: ",end="")
-
-	while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
-		
-		if(int(binary_array_A[calculation_counter])==0 and int(binary_array_B[calculation_counter])==0):
-			binary_X=1
-		elif(int(binary_array_A[calculation_counter])==1 and int(binary_array_B[calculation_counter])==1):
-			binary_X=1
-		else:
-			binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
+		while(calculation_counter!=len(binary_array_A) ):
+			binary_X=int(binary_array_A[calculation_counter])
 			if(binary_X==1):
+				binary_X=0
+			elif(binary_X==0):
+				binary_X=1
+
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
+
+	
+
+	elif (gate=="NAND" or gate=="nand" or gate=="Nand"):
+		formula="X = A * B (Inverted) "
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
+
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
+			binary_X=int(binary_array_A[calculation_counter])*int(binary_array_B[calculation_counter])
+			if(binary_X==1):
+				binary_X=0
+			elif(binary_X==0):
+				binary_X=1
+
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
+	
+
+	elif (gate=="NOR" or gate=="nor" or gate=="Nor"):
+		formula="X = A + B  (Inverted)"
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
+
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
+			binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
+			if binary_X==2:
+				binary_X=0
+			elif binary_X==1:
 				binary_X=0
 			else:
 				binary_X=1
+
+				print(binary_X,end="")
+				calculation_counter=calculation_counter+1
+	
+
+	elif (gate=="XOR" or gate=="xor" or gate=="Xor"):
+		formula="X = A + B (Exclusive) "
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
+
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
+			if(int(binary_array_A[calculation_counter])==1 and int(binary_array_B[calculation_counter])==1):
+				binary_X=0
+			elif(int(binary_array_A[calculation_counter])==0 and int(binary_array_B[calculation_counter])==0):
+				binary_X=0
+			else:
+				binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
+		
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
+	
+	
+
+
+	elif (gate=="XNOR" or gate=="xnor" or gate=="Xnor"):
+		formula="X = A + B (Inverted Exclusive)"
+		calculation_counter=0
+		print("")
+		print("Gate 		: ",gate.upper())
+		print("")
+		print("Formula 	: ",formula)
+		print("")
+		print("X 		: ",end="")
+
+		while(calculation_counter!=len(binary_array_A) or calculation_counter!=len(binary_array_B)):
+		
+			if(int(binary_array_A[calculation_counter])==0 and int(binary_array_B[calculation_counter])==0):
+				binary_X=1
+			elif(int(binary_array_A[calculation_counter])==1 and int(binary_array_B[calculation_counter])==1):
+				binary_X=1
+			else:
+				binary_X=int(binary_array_A[calculation_counter])+int(binary_array_B[calculation_counter])
+				if(binary_X==1):
+					binary_X=0
+				else:
+					binary_X=1
 		
 		
 		
 
-		print(binary_X,end="")
-		calculation_counter=calculation_counter+1
+			print(binary_X,end="")
+			calculation_counter=calculation_counter+1
+	
 	
 	print("")
 	print("")
+	end = input("End Program ? (yes/no) : ")
+	print("")
+	print("")
+
